22d2def792df7c45c3ffb3c79d5f0d31a2480453
Fatema <fatema53688@gmail.com>
2026-08-13 21:19:26 +0600

    array properties,math operations,indexing

---
+++ numpy/arrayProperties/convertDataType.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([1.2,2.5,3.8])
+print(arr.dtype)
+#convert from one data type to another
+int_arr = arr.astype(int)
+print(int_arr)
+print(int_arr.dtype)
---
+++ numpy/arrayProperties/dataType.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array([10,20,30.5,40])
+print(arr.dtype) # output float 64 means there exist intiger also
